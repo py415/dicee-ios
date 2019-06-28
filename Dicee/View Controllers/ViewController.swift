2532fd9a6a65a1399b68b4ecc3d3579ee2af53b0
Philip Yu <pyu3@mail.sfsu.edu>
2019-06-28 13:02:12 -0700

    Added Dice Randomization upon clicking Roll button

--- Dicee/View Controllers/ViewController.swift
+++ Dicee/View Controllers/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Properties
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
+    // Outlets
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateDiceImages()
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        // This will get executed when roll button is pressed
+        updateDiceImages()
+        
+    }
+    
+    func updateDiceImages() {
+        
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+    }
+    
 }
 
